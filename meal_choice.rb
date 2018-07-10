9e9a222b54544b7e1cd0f9abb85c3ba7c762fc26
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-10 17:53:07 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,1 +1,14 @@
-def 
+def breakfast(food = frosted flakes)
+  puts "Morning is the best time for #{food}"
+end
+breakfast
+
+def lunch(food = grilled cheese)
+  puts "Afternoon is the best time for #{food}"
+end
+lunch
+
+def dinner(food = salmon)
+  puts "Night is the best time for #{food}"
+end
+dinner
